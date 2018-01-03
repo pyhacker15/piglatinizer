e6139cd4a569a160abebe85427c0624d93349b3e
Louis Gabrie <louisgabrie1450@gmail.com>
2018-01-03 23:20:13 +0000

    I added things

--- js/script.js
+++ js/script.js
@@ -25,3 +25,25 @@ $( document ).ready(function() {
 	//It should return a transfromed sentance
 
 
+
+// $(document).ready(function() { 
+//     $("button").click(function () { 
+//         var userInput = $("input").val();
+//         var pigLatin = pigLatinizer(userInput);
+//         $("div").html(pigLatin);
+// });    
+//   });
+  
+// function pigLatinizer(word) { 
+//      var vowels =["a", "e", "i", "o", "u"];
+//      var firstLetter = word[0]; 
+//      var indexOf = vowels.indexOf(firstLetter);
+//      var beginsWithVowel = vowels.indexOf(firstLetter);
+//      console.log("index of" + indexOf);
+//      var beginsWithVowel = indexOf > -1; 
+//      if(beginsWithVowel) { 
+//          return word + "yay";
+//      } else  { 
+//       return word + "ay"; 
+//      } 
+// }
