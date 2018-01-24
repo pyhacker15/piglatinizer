152dee5c283af02d68657a08be442d18c654b0b7
Louis Gabrie <louisgabrie1450@gmail.com>
2018-01-24 02:29:15 +0000

    I'm saving, just in case if I didn't already, and I am currently testing slice();

--- js/script.js
+++ js/script.js
@@ -4,13 +4,6 @@
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkay.
 // CREATE THE FUNCTIONS BELOW
 
-// Document Ready Function. All of your jQuery should go in here. 
-$( document ).ready(function() {
-  
-
-
-
-});
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
@@ -21,29 +14,55 @@ $( document ).ready(function() {
 
 
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
+//Loops through all the words in the sentence and transforms each word
+//It should return a transfromed sentance
+
+$(document).ready(function() {
+     var wordCounts = {}; 
+     $("#userInput").keyup(function() {
+         var matches = this.value.match(/\b/g);
+         wordCounts[this.id] = matches ? matches.length / 2 : 0;
+         var finalCount = 0; 
+         $.each(wordCounts, function(k, v) {
+             finalCount += v; 
+         });
+         $('#display_count').html(finalCount);
+             am_cal(finalCount);
+     }).keyup();
+}); 
 
 
 
-// $(document).ready(function() { 
-//     $("button").click(function () { 
-//         var userInput = $("input").val();
-//         var pigLatin = pigLatinizer(userInput);
-//         $("div").html(pigLatin);
-// });    
-//   });
-  
-// function pigLatinizer(word) { 
-//      var vowels =["a", "e", "i", "o", "u"];
-//      var firstLetter = word[0]; 
-//      var indexOf = vowels.indexOf(firstLetter);
-//      var beginsWithVowel = vowels.indexOf(firstLetter);
-//      console.log("index of" + indexOf);
-//      var beginsWithVowel = indexOf > -1; 
-//      if(beginsWithVowel) { 
-//          return word + "yay";
-//      } else  { 
-//       return word + "ay"; 
-//      } 
-// }
+$(document).ready(function() { 
+    $("#translate").click(function () { // Creates click handler for the "Latinizer" button. 
+         var userInput = $("#userInput").val(); // Takes the users input and places it in a variable
+         var pigLatin = pigLatinizer(userInput); // Takes the users input and connects it to the pigLatinizer(word) func whereas "word" is the usersinput. 
+         $("#translation").html(pigLatin); // This allows the usersinput to be displayed on the screen
+     });    
+});
+ 
+
+function pigLatinizer(word) { 
+     var vowels =["a", "e", "i", "o", "u"]; 
+     var firstLetter = word[0]; // Takes the first letter of the usersinput and places it in a variable 
+     var indexOf = vowels.indexOf(firstLetter); // This checks the usersinput to see if the first letter begins with a vowel 
+     var beginsWithVowel = indexOf > -1; // This creates the conditional for the first letter
+     //word = word.slice(1); //Caution
+     if(beginsWithVowel) { 
+         return word + "yay";
+     } else { 
+         return word + "ay";
+         // Here edit the code so that if the word dosn't begin with vowel, then the first letter will be appended to the end of the word. 
+     } 
+}
+
+// function ToPigLatin() { 
+//       pigLatin.split("");  
+// } 
+
+// If hello is placed in the input field, then the output will be "helloay"
+// Else, if apple is placed in the input field, then the output will be "appleyay" 
+
+var letters = word.slice(""); // Slices the usersinput into an array  
+var firstLetter2 = letters[0]; // Takes the first letter of the usersinput 
+
