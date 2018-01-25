02a10c11c29fc5cc20ac86f8af842d89157778c9
Louis Gabrie <louisgabrie1450@gmail.com>
2018-01-25 15:18:52 +0000

    I updated the sentence functionlity with the pigLatinizer, and I cleaned up the code. This is the final commitfor the project, as of 1-2-18

--- js/script.js
+++ js/script.js
@@ -1,68 +1,79 @@
-// Pig Latin takes the first consonant (or consonant cluster) of an English 
-// word, moves it to the end of the word and suffixes an ay, or if a word
-// begins with a vowel you just add ay to the end. For example, pig becomes
-// igpay, banana becomes ananabay, and aadvark becomes aadvarkay.
-// CREATE THE FUNCTIONS BELOW
-
-
-
-// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
-
-
-
-
-
-
-// Create the sentenceToPigLatin function that takes a sentence as a parameter
-//Loops through all the words in the sentence and transforms each word
-//It should return a transfromed sentance
-
-$(document).ready(function() {
-     var wordCounts = {}; 
-     $("#userInput").keyup(function() {
-         var matches = this.value.match(/\b/g);
-         wordCounts[this.id] = matches ? matches.length / 2 : 0;
-         var finalCount = 0; 
-         $.each(wordCounts, function(k, v) {
-             finalCount += v; 
-         });
-         $('#display_count').html(finalCount);
-             am_cal(finalCount);
-     }).keyup();
-}); 
-
-
-
-$(document).ready(function() { 
-    $("#translate").click(function () { // Creates click handler for the "Latinizer" button. 
-         var userInput = $("#userInput").val(); // Takes the users input and places it in a variable
-         var pigLatin = pigLatinizer(userInput); // Takes the users input and connects it to the pigLatinizer(word) func whereas "word" is the usersinput. 
-         $("#translation").html(pigLatin); // This allows the usersinput to be displayed on the screen
-     });    
-});
- 
-
-function pigLatinizer(word) { 
-     var vowels =["a", "e", "i", "o", "u"]; 
-     var firstLetter = word[0]; // Takes the first letter of the usersinput and places it in a variable 
-     var indexOf = vowels.indexOf(firstLetter); // This checks the usersinput to see if the first letter begins with a vowel 
-     var beginsWithVowel = indexOf > -1; // This creates the conditional for the first letter
-     //word = word.slice(1); //Caution
-     if(beginsWithVowel) { 
-         return word + "yay";
-     } else { 
-         return word + "ay";
-         // Here edit the code so that if the word dosn't begin with vowel, then the first letter will be appended to the end of the word. 
-     } 
+function sentenceToPigLatin( sentence ) {
+	var words = sentence.split( " " );
+	var answers = [];
+	for ( var i = 0; i < words.length; i++ ) {
+		answers.push( pigLatinizer( words[ i ] ) );
+	}
+	return answers.join( " " );
 }
 
-// function ToPigLatin() { 
-//       pigLatin.split("");  
-// } 
+function sentenceToEnglish( sentence ) {
+	var words = sentence.split( " " );
+	var answers = [];
+	for ( var i = 0; i < words.length; i++ ) {
+		answers.push( englishConverter( words[ i ] ) );
+	}
+	return answers.join( " " );
+}
 
-// If hello is placed in the input field, then the output will be "helloay"
-// Else, if apple is placed in the input field, then the output will be "appleyay" 
+$( document ).ready( function() {
+	$( "#translateButton" ).click( function() {
+		var userInput = $( "#userInput" ).val();
+		var pigLatin = sentenceToPigLatin( userInput.toLocaleLowerCase( 1 ) );
+		$( "#userOutput" ).val( pigLatin );
+		$( "#userInput" ).val( " " );
+	} );
 
-var letters = word.slice(""); // Slices the usersinput into an array  
-var firstLetter2 = letters[0]; // Takes the first letter of the usersinput 
+	$( "#convertButton" ).click( function() {
+		var output = sentenceToEnglish( $( "#userOutput" ).val() );
+		$( "#userInput" ).val( output );
+		$( "#userOutput" ).val( " " );
+	} );
 
+	var wordCounts = {};
+	$( "#userInput" ).keyup( function() {
+		var matches = this.value.match( /\b/g );
+		wordCounts[ this.id ] = matches ? matches.length / 2 : 0;
+		var finalCount = 0;
+		$.each( wordCounts, function( k, v ) {
+			finalCount += v;
+		} );
+		$( '#display_count' ).html( finalCount );
+	} ).keyup();
+
+	var wordCounts1 = {};
+	$( "#userOutput" ).keyup( function() {
+		var matches = this.value.match( /\b/g );
+		wordCounts1[ this.id ] = matches ? matches.length / 2 : 0;
+		var finalCount = 0;
+		$.each( wordCounts1, function( k, v ) {
+			finalCount += v;
+		} );
+		$( '#display_count2' ).html( finalCount );
+	} ).keyup();
+} );
+
+
+function pigLatinizer( word ) {
+	var vowels = [ "a", "e", "i", "o", "u" ];
+	var firstLetter = word[ 0 ];
+	var indexOf = vowels.indexOf( firstLetter );
+	var beginsWithVowel = indexOf > -1;
+	if ( beginsWithVowel ) {
+		return word + "yay";
+	} else {
+		word = word.slice( 1 );
+		return word + firstLetter + "ay";
+	}
+}
+
+function englishConverter( word ) {
+	if ( word.slice( word.length - 3 ) !== "yay" ) {
+		var slicedWord = word.slice( 0, word.length - 2 );
+		var lastLetter = slicedWord[ slicedWord.length - 1 ];
+		var rest = slicedWord.slice( 0, slicedWord.length - 1 );
+		return lastLetter.concat( rest );
+	} else {
+		return word.slice( 0, word.length - 3 );
+	}
+}
